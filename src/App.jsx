c7cfd867b74b9135e342d69f4e736ae1a136bf68
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-12-11 03:11:04 +0000

    initial Commit for tailwind integration and some component creation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-gray-200 text-center">
+      <div className="bg-white rounded-lg shadow-lg p-4 transition-transform duration-200 w-80 mx-auto mt-5 text-center">
+        Hello
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
